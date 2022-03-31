28c78bc7f0b12ed760799337b9ac252dd54d5ed4
ozodbek-developer <ozodbekyusupov.developer@gmail.com>
2022-03-31 13:21:32 +0500

    123

---
+++ precache-manifest.a56d3de1c879b77022191d168b32d5d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb96da35ea12015986c09a3292952af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8016416d63cbdc6e99e4",
+    "url": "/static/css/main.d5af4fb8.chunk.css"
+  },
+  {
+    "revision": "9f16669a78388b1c7e2d",
+    "url": "/static/js/2.479629ae.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.479629ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8016416d63cbdc6e99e4",
+    "url": "/static/js/main.a10f4d73.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
